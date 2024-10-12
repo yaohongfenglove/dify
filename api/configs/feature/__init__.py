34ce90f53c2f96996fbb95408ec6428b76c53a6b
-LAN- <laipz8200@outlook.com>
2024-10-12 16:12:01 +0800

    feat(auth): update access token expiration time to 8 minutes for test

--- api/configs/feature/__init__.py
+++ api/configs/feature/__init__.py
@@ -392,7 +392,7 @@ class AuthConfig(BaseSettings):
 
     ACCESS_TOKEN_EXPIRE_MINUTES: PositiveInt = Field(
         description="Expiration time for access tokens in minutes",
-        default=60,
+        default=8,
     )
 
 
